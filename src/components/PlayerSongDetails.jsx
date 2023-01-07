e501fb6a7037429fc606eb30f4a6eb574f245a7f
Andrew <andriyilko@gmail.com>
2023-01-08 00:42:35 +0200

    Add music array

---
+++ src/components/PlayerSongDetails.jsx
@@ -0,0 +1,37 @@
+import { Paper } from "@mui/material";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import React, { useState } from "react";
+
+const PlayerSongDetails = () => {
+  const [img, setImg] = useState(
+    "https://i.scdn.co/image/ab67616d0000485188367cde4312ea2b32225e1b"
+  );
+  return (
+    <Paper
+      style={{
+        minWidth: "180px",
+        width: "30%",
+        backgroundColor: "transparent",
+        display: "flex",
+        alignItems: "center",
+      }}
+      elevation={0}
+      square
+    >
+      <div className="about-song text-white">
+        <img src={img} />
+        <div className="about-song__text-info">
+          <div className="text-sm">Miss You</div>
+          <div className="text-[11px] text-[#b3b3b3b3] font-medium">
+            Oliver Tree, Robin Schultz
+          </div>
+        </div>
+        <FavoriteBorderOutlinedIcon
+          style={{ transform: "scale(0.8)", marginLeft: "5px" }}
+        />
+      </div>
+    </Paper>
+  );
+};
+
+export default PlayerSongDetails;
---
+++ src/music-files/music.js
@@ -0,0 +1,26 @@
+import TearsInTheRainSong from "../music-files/tearsInTheRain.mp3";
+import MissYouSong from "../music-files/worthNothing.mp3";
+
+import defaultPicture from "../music-files/profile-img.avif";
+
+const TearsInTheRain = {
+  title: "Tears In The Rain",
+  author: "Hans Zimmer",
+  url: TearsInTheRainSong,
+  picture: defaultPicture,
+};
+
+const MissYou = {
+  title: "Miss You",
+  author: "Oliver Tree, Robin Schultz",
+  url: MissYouSong,
+  picture: defaultPicture,
+};
+
+// const = {
+//   title:"",
+//   author:"",
+//   url:
+// }
+
+export default [TearsInTheRain, MissYou];
